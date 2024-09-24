43083ff7ee3a0d3b159fc5716483287dd0311a26
sallyw02 <167825335+sallyw02@users.noreply.github.com>
2024-09-24 15:41:58 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():  # hassle to return whole html -> need ninja template
+    name = {'username': 'root'}
+    posts = [
+        {
+            'author': {'username': 'root'},
+            'body': "hi I'm root!"
+        },
+        {
+            'author': {'username': 'test'},
+            'body': "hi I'm test!"
+        },
+    ]
+    return render_template('index.html', name=name, posts=posts)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
